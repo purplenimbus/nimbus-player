efd395119616a6733250b2fc0a7e441fc86ee1e2
purplenimbus <you@example.com>
2017-09-17 00:59:54 +0100

    update web.js

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var gzippo = require('gzippo');
 var express = require('express');
 var logger = require('morgan');
 
-console.log(logger);
-console.log(logger('dev'));
+//console.log(logger);
+//console.log(logger('dev'));
 
 var app = express();
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
