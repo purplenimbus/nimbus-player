1548339beeb83385ba5cd9e22993b876193c3f50
purplenimbus <you@example.com>
2017-09-17 19:40:27 +0100

    add login and other filler elements to sidebar

--- app/scripts/controllers/player.js
+++ app/scripts/controllers/player.js
@@ -8,154 +8,157 @@
  * Controller of the nimbusPlayerApp
  */
 angular.module('nimbusPlayerApp')
-  .controller('PlayerCtrl', function ($scope,wave,music,waveForm,genius,artist,$window) {
+	.controller('PlayerCtrl', function ($scope,wave,music,waveForm,genius,artist,$window) {
 	
-	$scope.player = {
-	  currentSong: 0,
-	  currentTime: 0,
-	  playlist: music.songs
-	};
-	
-	
-    $scope.prevSong = function() {
+		$scope.loggedin = false;
 		
-      $scope.player.wave.stop();
-      console.log('Playing?',$scope.playing);
-		//stop current song
-		if($scope.playing){ $scope.stopSong(); }
-		//move pointer
-		$scope.player.currentSong -= 1;
-		//play song
-		$scope.playSong($scope.player.currentSong);
-    };
+		$scope.player = {
+		  currentSong: 0,
+		  currentTime: 0,
+		  playlist: music.songs
+		};
+	
+	
+		$scope.prevSong = function() {
+			
+		  $scope.player.wave.stop();
+		  console.log('Playing?',$scope.playing);
+			//stop current song
+			if($scope.playing){ $scope.stopSong(); }
+			//move pointer
+			$scope.player.currentSong -= 1;
+			//play song
+			$scope.playSong($scope.player.currentSong);
+		};
   
-    $scope.nextSong = function() {
-		
-      $scope.player.wave.stop();
-      console.log('Playing?',$scope.playing);
-		//stop current song
-		if($scope.playing){ $scope.stopSong(); }
-		//move pointer
-		$scope.player.currentSong += 1;
-		//play song
-		$scope.playSong($scope.player.currentSong);
-    };
+		$scope.nextSong = function() {
+			
+		  $scope.player.wave.stop();
+		  console.log('Playing?',$scope.playing);
+			//stop current song
+			if($scope.playing){ $scope.stopSong(); }
+			//move pointer
+			$scope.player.currentSong += 1;
+			//play song
+			$scope.playSong($scope.player.currentSong);
+		};
 
-    $scope.playSong = function(index) {
-		
-		console.log('index',index);
-      
-		//stop current song
-		if($scope.playing){
-			if(index === $scope.player.currentSong){
-				console.log('same song playing paused!');
-				$scope.pauseSong();
+		$scope.playSong = function(index) {
+			
+			console.log('index',index);
+		  
+			//stop current song
+			if($scope.playing){
+				if(index === $scope.player.currentSong){
+					console.log('same song playing paused!');
+					$scope.pauseSong();
+				}else{
+					console.log('diffrent song playing stopped!');
+					$scope.stopSong();
+					//play new index
+					$scope.player.songs[index].howler.play();
+					//set currentSong to new index
+					$scope.player.currentSong = index;
+					
+					$scope.playing = true;
+			
+			$scope.player.wave.start();
+		   // $scope.startTimer($scope.player.songs[index].howler.duration());
+				}
 			}else{
-				console.log('diffrent song playing stopped!');
-				$scope.stopSong();
 				//play new index
 				$scope.player.songs[index].howler.play();
 				//set currentSong to new index
 				$scope.player.currentSong = index;
 				
-				$scope.playing = true;
-        
-        $scope.player.wave.start();
-       // $scope.startTimer($scope.player.songs[index].howler.duration());
+				$scope.player.wave.start();
+		  
+	  //$scope.startTimer($scope.player.songs[index].howler.duration());
 			}
-		}else{
-			//play new index
-			$scope.player.songs[index].howler.play();
-			//set currentSong to new index
-			$scope.player.currentSong = index;
-			
-			$scope.player.wave.start();
-      
-  //$scope.startTimer($scope.player.songs[index].howler.duration());
-		}
-      
-    };
-    $scope.stopSong = function() {
-      console.log('pauseSong');
-      $scope.playing = false;
-      $scope.player.songs[$scope.player.currentSong].howler.stop();
-	  $scope.player.currentTime = 0;
-      
-      $scope.player.wave.stop();
-    };
-  
-	$scope.pauseSong = function() {
-      console.log('pauseSong');
-      $scope.playing = false;
-      $scope.player.songs[$scope.player.currentSong].howler.pause();
-    
-    $scope.player.wave.stop();
-    
-    };
-
-	$scope.playerInit = function(){
-		//init howler;
+		  
+		};
 		
-		$scope.playing = false;
-	
-		$scope.player = {
-		  /*currentSong: 0,
-		  currentTime: 0,
-		  playlist: music.songs()*/
+		$scope.stopSong = function() {
+		  console.log('pauseSong');
+		  $scope.playing = false;
+		  $scope.player.songs[$scope.player.currentSong].howler.stop();
+		  $scope.player.currentTime = 0;
+		  
+		  $scope.player.wave.stop();
+		};
+	  
+		$scope.pauseSong = function() {
+		  console.log('pauseSong');
+		  $scope.playing = false;
+		  $scope.player.songs[$scope.player.currentSong].howler.pause();
+		
+		$scope.player.wave.stop();
+		
+		};
+
+		$scope.playerInit = function(){
+			//init howler;
+			
+			$scope.playing = false;
+		
+			$scope.player = {
+			  /*currentSong: 0,
+			  currentTime: 0,
+			  playlist: music.songs()*/
+			};
+		
+			$scope.player.songs = music.initHowl(music.songs,$scope);
+		
+			$scope.player.wave = wave.initWave($scope);
+		
+			$scope.player.currentSong = 0;
+		
+			$scope.offCanvas = $window.UIkit.offcanvas('#off-canvas');
+				
+			angular.element('#off-canvas').on('hidden',function(){
+				$scope.resetOffCanvas();
+			});
+
+			console.log('Songs',$scope);
+		
 		};
 	
-		$scope.player.songs = music.initHowl(music.songs,$scope);
-    
-		$scope.player.wave = wave.initWave($scope);
-	
-		$scope.player.currentSong = 0;
-    
-		$scope.offCanvas = $window.UIkit.offcanvas('#off-canvas');
+		$scope.resetOffCanvas = function(){
 			
-		angular.element('#off-canvas').on('hidden',function(){
-			$scope.resetOffCanvas();
-		});
-
-		console.log('Songs',$scope);
-
-    
-	};
-	
-	$scope.resetOffCanvas = function(){
-		
-		$scope.playlist = false;
-		$scope.offCanvas.description = false;
-		$scope.offCanvas.title = false;
-		
-		console.log('offcanvas reset');
-	};
+			$scope.playlist = false;
+			$scope.offCanvas.description = false;
+			$scope.offCanvas.title = false;
+			$scope.offCanvas.profile = false;
+			$scope.offCanvas.search = false;
+			console.log('offcanvas reset');
+		};
 	
 	  
-	  $scope.getInfo = function(id,name){
-		$scope.offCanvas.show();
-		
-		var info = artist.getInfo(id,name,$scope);
+		$scope.getInfo = function(id,name){
+			$scope.offCanvas.show();
 			
-		$scope.offCanvas.title = name;
-		
-		if(info.status === 200){
-		  $scope.offCanvas.description = info.data;
-		} else{
-		  $scope.offCanvas.description = 'no information found';
-		}
-	  };
+			var info = artist.getInfo(id,name,$scope);
+				
+			$scope.offCanvas.title = name;
+			
+			if(info.status === 200){
+			  $scope.offCanvas.description = info.data;
+			} else{
+			  $scope.offCanvas.description = 'no information found';
+			}
+		};
 	  
-	  $scope.showPlaylist = function(){
+		$scope.showPlaylist = function(){
 		
-		console.log('show playlist',$scope);
+			console.log('show playlist',$scope);
+			
+			$scope.offCanvas.show();
+			
+			$scope.offCanvas.title = name;
+			
+			$scope.playlist = true;
 		
-		$scope.offCanvas.show();
-		
-		$scope.offCanvas.title = name;
-		
-		$scope.playlist = true;
-		
-	  };
+		};
 	  
 		$scope.showProfile = function(){
 		
@@ -163,9 +166,9 @@ angular.module('nimbusPlayerApp')
 		
 			$scope.offCanvas.show();
 		
-			$scope.offCanvas.title = 'account';
+			$scope.offCanvas.title = $scope.loggedin ? 'account' : 'login';
 		
-			$scope.profile = true;
+			$scope.offCanvas.profile = true;
 		
 		};
 		
@@ -177,8 +180,12 @@ angular.module('nimbusPlayerApp')
 		
 			$scope.offCanvas.title = 'Search';
 		
-			$scope.search = true;
+			$scope.offCanvas.search = true;
 		
+		};
+		
+		$scope.login = function(data){
+			console.log('login data',data);
 		};
 
 		$scope.playerInit();
