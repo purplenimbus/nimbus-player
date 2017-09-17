5f56ef1fee39fe7bd39a4c3e18aa82012aa3c4e1
purplenimbus <you@example.com>
2017-09-17 01:00:54 +0100

    remove morgan module

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var logger = require('morgan');
+//var logger = require('morgan');
 
 //console.log(logger);
 //console.log(logger('dev'));
